554f44ac32b58469d4a385ab4f161492fdc3ab9b
Aldona <aldona.tomaszewska@gmail.com>
2021-04-29 11:44:33 +0200

    pierwszy komit

---
+++ PythonZadania.py
@@ -0,0 +1,14 @@
+# a = 10
+# Każdej z nastepujacych wartosci przypisac do niezaleznej zmiennej.
+# • Pole okregu 1 o promieniu a.
+# • Obwod okregu 1 o promieniu a.
+import math
+def poleOkregu(a):
+    pole=a*a*math.pi
+    print(pole)
+
+
+poleOkregu(10)
+
+
+
