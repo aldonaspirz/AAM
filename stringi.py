efe857bb5f024b7d90143f0958d42441a0b84e40
Aldona <aldona.tomaszewska@gmail.com>
2021-04-30 15:07:44 +0200

    zadanie 1,2,3

--- stringi.py
+++ stringi.py
@@ -8,5 +8,31 @@ def iteracja(zmienna):
             words.append(j)
             return words[0]
 
+def iteracja_1(zmienna):
+    for i in zmienna:
+        words=[]
+
+        for j in i:
+        
+            words.append(j)
+        return words[-1]
+
+def iteracja_2(zmienna):
+    for i in zmienna:
+        words=[]
+        a=0
+
+        for j in i:
+            
+            words.append(j)
+        a=len(words)
+        
+
+        return words[int(a/2)]
+
+
 wynik=iteracja(['konstantynopolitańczykowianeczka'])
-print(wynik)
+wynik_1=iteracja_1(['konstantynopolitańczykowianeczka'])
+wynik_2=iteracja_2(['konstantynopolitańczykowianeczka'])
+
+print(wynik, wynik_1, wynik_2)
