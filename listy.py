27a64bb5716c31da60be5b995bd5f7c7dee0f96a
Aldona <aldona.tomaszewska@gmail.com>
2021-05-21 10:58:33 +0200

    poprawki

--- listy.py
+++ listy.py
@@ -1,5 +1,9 @@
 # . Stworz liste (A) skladajaca sie z 11 liczb calkowitych.
 A=[1,2,3,4,5,6,7,8,9,10,11]
+A1 = [1,2,3]
+B = [4,5,6]
+C = A1 + B
+print(C)
 # Rozszerz tę listę o 10 liczb calkowitych.
 A.extend(range(12,22))
 # Zamien czesc ujemną z pierwszej listy na 5 pierwszych liczb z listy B.
@@ -24,20 +28,25 @@ def zadanie6(zmienna):
             list.append(i)
     return list
 
+def zadanie6michala(zmienna):
+    del zmienna[:2]
+    del zmienna[2:5]
+    return zmienna
+wynik6m=zadanie6michala(A)
 wynik6=zadanie6(A)
-print(wynik6)
+print(wynik6,wynik6m)
 
 # Stworz 5-elementową liste ktora zawiera dowolne litery.
 import string
 import random
-def zadanie7():
+def zadanie7(argument):
     list=[]
-    for i in range(5):
+    for i in range(argument):
         random_letter = random.choice(string.ascii_letters)
         list.append(random_letter)
         
     return list
-wynik7=zadanie7()
+wynik7=zadanie7(5)
 print(wynik7)
 lista_new=[]
 wynik7.reverse()
