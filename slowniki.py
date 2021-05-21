8b8275e1447a7f560fed6e2ff06b0090e919a618
Aldona <aldona.tomaszewska@gmail.com>
2021-05-21 11:27:16 +0200

    start

---
+++ slowniki.py
@@ -0,0 +1,14 @@
+# 1. Stwórz słownik w którym kluczami będą ulubione napoje (bezalkoholowe), a wartosciami
+# ich ocena w skali od 0 do 10.
+bezalko
+
+
+
+
+
+
+
+
+
+
+A={'piwo':10,'wino czerwone':9,'wino biale':8, 'whiskey':7,}
