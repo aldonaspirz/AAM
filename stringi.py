c277094e70fa495785809c1f1b87c658dd24279f
Aldona <aldona.tomaszewska@gmail.com>
2021-05-21 11:11:02 +0200

    do sprawdzenia

--- stringi.py
+++ stringi.py
@@ -1,4 +1,4 @@
-
+# 1. pierwsza literę.
 def iteracja(zmienna):
     for i in zmienna:
         words=[]
@@ -7,7 +7,7 @@ def iteracja(zmienna):
         
             words.append(j)
             return words[0]
-
+# 2. ostatnia literę
 def iteracja_1(zmienna):
     for i in zmienna:
         words=[]
@@ -16,7 +16,7 @@ def iteracja_1(zmienna):
         
             words.append(j)
         return words[-1]
-
+# 3. środkowa literę
 def iteracja_2(zmienna):
     for i in zmienna:
         words=[]
@@ -29,7 +29,7 @@ def iteracja_2(zmienna):
         
 
         return words[int(a/2)]
-
+# 5. ostatnie cztery litery.
 def iteracja_3(zmienna):
     for i in zmienna:
         words=[]
@@ -42,7 +42,7 @@ def iteracja_3(zmienna):
         
 
         return words[-5:-1]
-
+# 4. pierwsze cztery litery
 def iteracja_4(zmienna):
     for i in zmienna:
         words=[]
@@ -55,6 +55,8 @@ def iteracja_4(zmienna):
         
 
         return words[0:4]
+
+# 6. litery od srodkowej od czwartej od konca.
 
 def iteracja_6(zmienna):
     for i in zmienna:
@@ -69,6 +71,7 @@ def iteracja_6(zmienna):
 
         return words[(int(a/2)):-4]
 
+# 7. uzywając liter 4 pierwszych i ostatnich stworz nowy ciag znakow
 def iteracja_7(zmienna):
     for i in zmienna:
         words=[]
@@ -78,16 +81,17 @@ def iteracja_7(zmienna):
             
             words.append(j)
         
-    return words[0:4], words[-4:-1]
+    return words[0:4] + words[-4:]
 
-        
+        # 8. zamien wszystkie litery w wyrazie na Wielkie litery
 def iteracja_8(zmienna):
     
     return zmienna.upper()
-
+# 9. zamien 4 pierwsze i ostatnie litery na Wielkie i stworz nowy wyraz aby odpowiadal tresci
+# słowu oryginalnemu.
 def iteracja_9(zmienna):
     
-    return zmienna.upper()
+    return zmienna[:4].upper() +zmienna[-4:].upper()
 
 
 wynik=iteracja(['konstantynopolitańczykowianeczka'])
@@ -98,5 +102,5 @@ wynik_4=iteracja_4(['konstantynopolitańczykowianeczka'])
 wynik_6=iteracja_6(['konstantynopolitańczykowianeczka'])
 wynik_7=iteracja_7(['konstantynopolitańczykowianeczka'])
 wynik_8=iteracja_8('konstantynopolitańczykowianeczka')
-
-print(wynik, wynik_1, wynik_2,wynik_3, wynik_4,wynik_6,wynik_7, wynik_8)
+wynik_9=iteracja_9('konstantynopolitańczykowianeczka')
+print(wynik, wynik_1, wynik_2,wynik_3, wynik_4,wynik_6,wynik_7, wynik_8,wynik_9)
