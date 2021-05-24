3094ee5625a91e1fceb58a648b5a180dfd040e7d
Aldona <aldona.tomaszewska@gmail.com>
2021-05-24 12:48:07 +0200

    przed zadaniem 9

--- slowniki.py
+++ slowniki.py
@@ -1,5 +1,8 @@
 # 1. Stwórz słownik w którym kluczami będą ulubione napoje (bezalkoholowe), a wartosciami
 # ich ocena w skali od 0 do 10.
+from typing import KeysView
+
+
 bezalko={'woda':10, 'cola':9, 'orange juice':8, 'lemoniada':7, 'woda gazowana':6, 'fanta':5, 'sprite':4, 'pinapple juice':3, 'piwo bezalkoholowe':2, 'sok porzeczkowy':1, 'pepsi':0 }
 # 2. Dodaj do słownika 5 napojow alkoholowych wraz z ich ocena
 A={'piwo':10,'wino czerwone':9,'wino biale':8, 'whiskey':7,'rum':6}
@@ -20,4 +23,17 @@ print(sorted(bezalko))
 print(sorted((bezalko),reverse=True))
 
 # 7. Posortuj oceny od najmniejszej do najwiekszej.
+print('rozwiazanie nr 7:',sorted((bezalko.items()),reverse=True,key=lambda x: x[1]))
 
+# 8. Stwórz nowy słownik, który bedzie miał przypisane rateingi od najmniejszego do największego do alfabetycznej kolejności nazw.
+def raitings(zmienna):
+    klucze=sorted(bezalko)
+    wartosci=sorted((bezalko.items()),key=lambda x: x[1])
+    
+    zip_iterator = zip(klucze, wartosci)
+    nowy_s=dict(zip_iterator)
+    return nowy_s
+wyniki8=raitings(bezalko)
+print(wyniki8)
+
+# 9. Stworz słownik który będzie zawierał wszystkie listy z ćwiczenia nr 3.
